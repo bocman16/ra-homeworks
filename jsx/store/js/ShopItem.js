6cf7c0cc0706f3c2554bbf0f275b531b80bb952d
bocman16 <bocman16@yandex.ru>
2019-03-16 20:32:57 +0300

    Задание 1 Готово!

---
+++ jsx/store/js/ShopItem.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const ShopItem = () => {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window  mobile">
+        <div className="highlight-overlay" />
+      </div>
+      <div className="divider" />
+      <div className="purchase-info">
+        <div className="price">£{item.price.toFixed(2)}</div>
+        <button>{item.currency}</button>
+      </div>
+    </div>
+  );
+};
