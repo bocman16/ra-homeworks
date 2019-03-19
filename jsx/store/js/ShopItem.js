449358976a9aae7a60f74b30aa9fdc0ffcadd25a
bocman16 <bocman16@yandex.ru>
2019-03-19 11:55:07 +0300

    Исправил

--- jsx/store/js/ShopItem.js
+++ jsx/store/js/ShopItem.js
@@ -2,20 +2,20 @@
 
 
 
-const ShopItem = itemList => {
+const ShopItem = props => {
   return (
     <div className="main-content">
-      <h2>{itemList.item.brand}</h2>
-      <h1>{itemList.item.title}</h1>
-      <h3>{itemList.item.description}</h3>
-      <div className="description">{itemList.item.descriptionFull}</div>
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
       <div className="highlight-window  mobile">
         <div className="highlight-overlay" />
       </div>
       <div className="divider" />
       <div className="purchase-info">
-        <div className="price">£{itemList.item.price.toFixed(2)}</div>
-        <button>{itemList.item.currency}</button>
+        <div className="price">£{props.item.price.toFixed(2)}</div>
+        <button>{props.item.currency}</button>
       </div>
     </div>
   );
